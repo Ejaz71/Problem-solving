00ab87cb9e2b67e2221f8e5a565f6b09527650cc
Ejaz71 <ejazchowdhury.buetynwa@gmail.com>
2025-08-31 21:35:14 +0600

    lcp using divide and conquer

--- 14. Longest Common Prefix/src/Solution.java
+++ 14. Longest Common Prefix/src/Solution.java
@@ -23,8 +23,35 @@ class Solution {
         return lcp.toString();
     }
 
+    public static String lcpDivideAndConquer(String[] strs) {
+        return lcpDNC(strs, 0, strs.length - 1);
+    }
+
+    private static String lcpDNC(String[] strs, int left, int right) {
+        if (left == right) return strs[left];
+        else if (left < right) {
+            int mid = left + (right - left) / 2;
+            String leftLCP = lcpDNC(strs, left, mid);
+            String rightLCP = lcpDNC(strs, mid + 1, right);
+            return lcp2Strings(leftLCP, rightLCP);
+        }
+        return "";
+    }
+
+    private static String lcp2Strings (String s1, String s2) {
+        StringBuilder lcp = new StringBuilder();
+        int n = Math.min(s1.length(), s2.length());
+        int i = 0;
+        while(i < n && s1.charAt(i) == s2.charAt(i)) {
+            lcp.append(s1.charAt(i));
+            i++;
+        }
+        return lcp.toString();
+    }
+
     public static void main(String[] args) {
-        String[] strs = {"flower"};
+        String[] strs = {"flower", "florian", "floasdfg", "flying", "abc"};
         System.out.println(longestCommonPrefix(strs));
+        System.out.println(lcpDivideAndConquer(strs));
     }
 }
